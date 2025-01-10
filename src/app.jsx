b26824c492dc221c01afadd19feb4277023d6ff5
Hadismrd18 <155313948+Hadismrd18@users.noreply.github.com>
2025-01-10 21:54:51 +0330

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -31,7 +31,7 @@ const clearList = ()=>{
           <img src={bgHeaderDesktop} className="min-[376px]:block " />
         </div>
         <div className="w-full h-full py-8 px-4 min-[1440px]:px-8 min-[1440px]:w-[75%] mx-auto relative">
-          <div className="absolute bg-white flex-wrap gap-10 w-[80%] h-auto sm:h-16 rounded  top-[-30px] left-[10%] flex items-center justify-between p-3">
+          <div className="absolute bg-white gap-10 w-[80%] h-auto sm:h-16 rounded  top-[-30px] left-[10%] flex items-center justify-between p-3">
             <div className="flex w-full gap-5">
               {skillData.map((skill) => {
                 return (
